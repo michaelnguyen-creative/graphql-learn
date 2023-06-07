e1b2e9efd68dc1b00e00a37b79d980924dcce29a
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-06-07 16:09:19 +0700

    add apollo server def

--- part1/server/src/index.js
+++ part1/server/src/index.js
@@ -1,1 +1,29 @@
-//TODO
+// Define & start an Apollo Server
+const { ApolloServer } = require("@apollo/server");
+// require startStandardServer function from @apollo/server
+const { startStandaloneServer } = require("@apollo/server/standalone");
+// Use mock schema for now with mocked data
+// require from @graphql-tools/mock and @graphql-tools/schema
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+// require typeDefs from schema.js
+const typeDefs = require("./schema");
+// require mocks from mock.js
+const mocks = require("./mocks");
+
+// start Apollo standalone server async lambda def
+const startApolloServer = async () => {
+  // Generate a mock schema with mock resolvers
+  const schemaWithMocks = addMocksToSchema({
+    schema: makeExecutableSchema({ typeDefs }),
+  });
+  // create a new ApolloServer instance, passing in the typeDefs & resolvers
+  const server = new ApolloServer({ schema: schemaWithMocks, mocks });
+  // start standalone server
+  const { url } = await startStandaloneServer(server);
+  // log the server url
+  console.log(`Apollo Server is running at ${url}`);
+};
+
+// start server
+startApolloServer();
