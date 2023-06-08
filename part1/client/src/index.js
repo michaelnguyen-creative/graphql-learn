04baace881c6a817318fe124ee687d37d3703b9e
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-06-08 18:55:18 +0700

    add displaying track cards on homepage feature

--- part1/client/src/index.js
+++ part1/client/src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
 
+// Apollo Client imports
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+// Instantiate Apollo Client
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+// Wrap the App in the ApolloProvider higher-order component
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- part1/client/src/pages/tracks.js
+++ part1/client/src/pages/tracks.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { useQuery, gql } from "@apollo/client";
+import { QueryResult } from "../components";
+import TrackCard from "../containers/track-card";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+// define TRACKS query
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  // useQuery hook returns an object with loading, error, & data properties
+  const { loading, error, data } = useQuery(TRACKS);
+  // return QueryResult nested in Layout
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
